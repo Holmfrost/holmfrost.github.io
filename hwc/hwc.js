f66191fe4bc958ce5eebb5594075b8935c8aa763
Holmfrost <holmfrost@proton.me>
2024-12-30 17:29:33 +0100

    A

---
+++ hwc/hwc.js
@@ -0,0 +1,6 @@
+const hwcCanvas = document.querySelector("hwcCanvas");
+const hwcCanvasContext = hwcCanvas.getContext("2d");
+
+hwcCanvasContext.fillStyle = "green";
+
+hwcCanvasContext.fillRect(10, 10, 100, 100);
