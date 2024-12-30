74387bf062a02513a4c319bb944595f60b3a5a94
Holmfrost <holmfrost@proton.me>
2024-12-30 17:35:41 +0100

    A

--- hwc/hwc.js
+++ hwc/hwc.js
@@ -1,6 +1,6 @@
-const hwcCanvas = document.querySelector("hwcCanvas");
-const hwcCanvasContext = hwcCanvas.getContext("2d");
+var hwcCanvas = document.getElementById("hwcCanvas");
+var hwcCanvasContext = hwcCanvas.getContext("2d");
 
-hwcCanvasContext.fillStyle = "green";
-
-hwcCanvasContext.fillRect(10, 10, 100, 100);
+hwcCanvasContext.moveTo(0, 0);
+hwcCanvasContext.lineTo(200, 100);
+hwcCanvasContext.stroke();
